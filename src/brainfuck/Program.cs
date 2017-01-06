98e0351ea3409834cd7c3c339e273cf14e89d523
jonathanvdc <jonathan.vdc@outlook.com>
2017-01-06 20:59:51 +0100

    Create a copy of the hello-world project

---
+++ src/brainfuck/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Flame.Front.Cli;
+using Flame.Front.Projects;
+
+namespace Flame.Brainfuck
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ProjectHandlers.RegisterHandler(new BrainfuckHandler());
+            var compiler = new ConsoleCompiler(
+                "flame-brainfuck", "my Flame-based Brainfuck compiler",
+                "https://github.com/jonathanvdc/flame-brainfuck/releases");
+            compiler.Compile(args);
+        }
+    }
+}
